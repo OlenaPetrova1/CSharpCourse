d802d0b74ffd75bdeaa374e0577d150739467d15
OlenaPetrova1 <Olena.Petrova@infopulse.com>
2023-04-18 13:59:41 +0300

    add homework3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        public delegate void DelegatePrintColorText(string text);
+        public delegate void DelegatePrintTextByColor(string text, ConsoleColor color);
+        static void Main(string[] args)
+        {
+            DelegatePrintColorText del1 = PrintGreen;
+            DelegatePrintColorText del2 = PrintMagenta;
+            del1.Invoke("Green text");
+            del2.Invoke("Magenta text");
+
+            DelegatePrintTextByColor del3 = Print;
+            del3.Invoke("Red TextByColor", ConsoleColor.Red);
+            del3.Invoke("Yellow TextByColor", ConsoleColor.Yellow);
+        }
+
+        static void PrintGreen(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+        }
+
+        static void PrintMagenta(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(msg);
+        }
+
+        static void Print(string msg, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+        }
+    }
+}
