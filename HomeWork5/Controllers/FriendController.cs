dc368218e34a0d271787899d64e2a75f5d8c888c
OlenaPetrova1 <Olena.Petrova@infopulse.com>
2023-04-26 11:35:50 +0300

    adding homework5

---
+++ HomeWork5/Controllers/FriendController.cs
@@ -0,0 +1,71 @@
+﻿using HomeWork5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork5.Controllers
+{
+    public class FriendController : Controller
+    {
+        public static List<Friend> Friends = new List<Friend>() 
+            { 
+                new Friend() { FriendId = 0, FriendName = "Diana", Place = "Kyiv" },
+                new Friend() { FriendId = 1, FriendName = "Sam", Place = "Helsinki" },
+                new Friend() { FriendId = 2, FriendName = "Mark", Place = "Berlin" },
+                new Friend() { FriendId = 3, FriendName = "Milana", Place = "Budapest" },
+                new Friend() { FriendId = 4, FriendName = "Robert", Place = "Monreal" }
+            };
+
+        public static int Id;
+
+        public IActionResult Index()
+        {
+             return View(Friends);
+        }
+
+        public IActionResult Insert()
+        {
+            return View("Insert");
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Friend friend)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            Friends.Add(friend);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var fr = Friends.Where(f => f.FriendId == id).FirstOrDefault();
+            Id = id;
+            return View(fr);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Friend friend)
+        {
+            var fr = Friends.Where(f => f.FriendId == Id).FirstOrDefault();
+            fr.FriendId = friend.FriendId;
+            fr.FriendName = friend.FriendName;
+            fr.Place = friend.Place;
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var fr = Friends.Where(f => f.FriendId == id).FirstOrDefault();
+            Id = id;
+            return View(fr);
+        }
+
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            var fr = Friends.Where(f => f.FriendId == Id).FirstOrDefault();
+            Friends.Remove(fr);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HomeWork5/Models/Friend.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeWork5.Models
+{
+    public class Friend
+    {
+        [Required(ErrorMessage = "Friend ID is required")]
+        public int FriendId { get; set; }
+
+        [Required (ErrorMessage = "Friend Name empty not allowed") ]
+        public string FriendName { get; set; }
+
+        [StringLength(25)]
+        public string Place { get; set; }
+
+    }
+}
